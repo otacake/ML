a0cd3d68e41121b5d90cd48fa840f38a6e8f50ff
otacake <shun142634-ku.mm@au.com>
2020-08-13 14:38:04 +0900

    8/13

--- sec3/decision_tree.py
+++ sec3/decision_tree.py
@@ -14,11 +14,11 @@ y = iris.target
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=1,stratify=y)
 
-from sklearn.tree import DecisionTreeClassifier #決定木のオブジェクト生成
-tree = DecisionTreeClassifier(criterion="gini",max_depth=4,random_state=1) #ジニ不純度を指標に、深さの最大を4までに設定、state指定して再現性を持たせる
-tree.fit(X_train,y_train) #学習作業
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(criterion="entropy",n_estimators=10,random_state=0,n_jobs=2)
+forest.fit(X_train,y_train)
 X_combined = np.vstack((X_train,X_test)) #下にくっつける、行列の形を考えれば自然
 y_combined = np.hstack((y_train,y_test))
 
-plot_decision_regions(X_combined,y_combined,classifier=tree,test_idx=range(105,150))
+plot_decision_regions(X_combined,y_combined,classifier=forest,test_idx=range(105,150))
 plt.show()
--- sec4/preprocessing.py
+++ sec4/preprocessing.py
@@ -9,10 +9,3 @@ csv_data = '''A,B,C,D
             10.0,11.0,12.0,'''
 
 df = pd.read_csv(StringIO(csv_data))
-print(df.T)
-
-from sklearn.impute import SimpleImputer
-imr = SimpleImputer(strategy="mean")
-imr=imr.fit(df.values)
-imputed_data = imr.transform(df.values)
-print(imputed_data)
