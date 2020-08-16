49342191dc5abdfddadea283c83a4a19e3a36c20
otacake <shun142634-ku.mm@au.com>
2020-08-16 23:06:27 +0900

    8/16

--- sec10/house_price.py
+++ sec10/house_price.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib
-
 import matplotlib.pyplot as plt
-from scipy.stats import skew
-from scipy.stats.stats import pearsonr
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/'
+                 'housing/housing.data',
+                 header=None,
+                 sep='\s+')
+
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+sns.set(style='whitegrid',context='notebook')
+cols = ["LSTAT","INDUS","RM","MEDV"]
+sns.pairplot(df[cols],height=3.5)
+plt.show()
